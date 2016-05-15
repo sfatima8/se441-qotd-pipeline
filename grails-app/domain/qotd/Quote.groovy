4b7a2f4cdb22c1bd890f40fbac5e75d5f3b821cc
sfatima8 <fatima.sarah@gmail.com>
2016-05-15 17:05:17 -0500

    c

--- grails-app/domain/qotd/Quote.groovy
+++ grails-app/domain/qotd/Quote.groovy
@@ -2,8 +2,11 @@ package qotd
 
 class Quote {
     String text;
+    
+    static belongsTo = [attribution:Attribution];
 
     static constraints = {
       text nullable: false
+      attribution nullable: true
     }
 }
